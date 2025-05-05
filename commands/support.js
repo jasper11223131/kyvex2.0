f0821680f83a60d467431e3d1ba044aa7ceb239d
jasper11223131 <163884717+jasper11223131@users.noreply.github.com>
2025-05-05 22:07:08 +0600

    Add files via upload

---
+++ commands/support.js
@@ -0,0 +1,49 @@
+const { EmbedBuilder } = require('discord.js');
+const config = require("../config.js");
+const musicIcons = require('../UI/icons/musicicons.js');
+
+module.exports = {
+    name: "support",
+    description: "Get support server link",
+    permissions: "0x0000000000000800",
+    options: [],
+    run: async (client, interaction, lang) => {
+        try {
+            const supportServerLink = "https://discord.gg/GyAabDe4Pk";
+            const githubLink = "https://github.com/";
+            const replitLink = "https://replit.com/";
+            const youtubeLink = "https://www.youtube.com/";
+
+            const embed = new EmbedBuilder()
+                .setColor('#b300ff')
+                .setAuthor({
+                    name: lang.support.embed.authorName,
+                    iconURL: musicIcons.beats2Icon, 
+                    url: config.SupportServer
+                })
+                .setDescription(lang.support.embed.description
+                    .replace("{supportServerLink}", supportServerLink)
+                    .replace("{githubLink}", githubLink)
+                    .replace("{replitLink}", replitLink)
+                    .replace("{youtubeLink}", youtubeLink)
+                )
+                .setImage('https://cdn.discordapp.com/attachments/1230822430973628496/1230822729695891497/7491-evil-wonderboy.png?ex=6819373e&is=6817e5be&hm=254d7608c77b44d82b81bd539a505cf5a569ce61e8c27be4645d20713682c1e8&')
+                .setTimestamp();
+
+            await interaction.reply({ embeds: [embed] });
+        } catch (e) {
+            console.error(e);
+            const errorEmbed = new EmbedBuilder()
+                .setColor('#ff0000')
+                .setAuthor({
+                    name: lang.support.embed.error,
+                    iconURL: musicIcons.alertIcon,
+                    url: config.SupportServer
+                })
+                .setDescription(lang.support.embed.errorDescription)
+                .setFooter({ text: lang.footer, iconURL: musicIcons.heartIcon });
+
+            await interaction.reply({ embeds: [errorEmbed], ephemeral: true });
+        }
+    },
+};
---
+++ config.js
@@ -0,0 +1,27 @@
+
+
+module.exports = {
+  TOKEN: "MTM0NTAwMDY4NTk1OTM4NTEwOA.GqQdJR.1pl1KqX_Y4e2owFT8sbuivB1oPF6gm9r8jsD2M",
+  language: "en",
+  ownerID: ["1089170457640570930", ""], 
+  mongodbUri : "mongodb+srv://djskyemusics:toto11223344551@cluster0.taexbjy.mongodb.net/?retryWrites=true&w=majority",
+  spotifyClientId : "a568b55af1d940aca52ea8fe02f0d93b",
+  spotifyClientSecret : "e8199f4024fe49c5b22ea9a3dd0c4789",
+  setupFilePath: './commands/setup.json',
+  commandsDir: './commands',  
+  embedColor: "#1db954",
+  activityName: "Kyvex | /help", 
+  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  SupportServer: "https://discord.gg/GyAabDe4Pk",
+  embedTimeout: 5, 
+  errorLog: "", 
+  nodes: [
+     {
+      name: "GlaceYT",
+      password: "youshallnotpass",
+      host: "lavalink.jirayu.net",
+      port:  13592,
+      secure: false
+    }
+  ]
+}
